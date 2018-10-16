25b5abcb4532b1d7b2ede283bb6dce5bb6969fe8
Nico Otremba <nico.otremba@mmi-media.de>
2018-10-16 18:23:38 +0200

    improved deserialization

--- OMCL/Serialization/Deserializer.cs
+++ OMCL/Serialization/Deserializer.cs
@@ -11,20 +11,29 @@ using OMCL.Data;
 namespace OMCL.Serialization {
 
 public interface IArrayConverter {
-    object CreateInstance();
-
-    void AddValue(object collection, object value);
     Type ElementType { get; }
+    object CreateInstance();
+    void AddValue(object collection, object value);
 }
 
-public class Deserializer {
+    public interface IStringConverter
+    {
+        object ConvertString(List<string> tags, string str);
+    }
+
+    public class Deserializer {
 
     private Dictionary<Type, IArrayConverter> _arrayConverters = new Dictionary<Type, IArrayConverter>();
+    private Dictionary<Type, IStringConverter> _stringConverters = new Dictionary<Type, IStringConverter>();
 
     public Deserializer() {}
     
     public void AddArrayConverter<T>(IArrayConverter conv) {
         _arrayConverters[typeof(T)] = conv;
+    }
+
+    public void AddStringConverter<T>(IStringConverter conv) {
+        _stringConverters[typeof(T)] = conv;
     }
 
     public T Deserialize<T>(Parser parser)
@@ -35,76 +44,6 @@ public class Deserializer {
         FillFields(result, obj);
 
         return result;
-    }
-
-    public object Deserialize(Type type, OMCLObject obj) {
-        // check if its a dictionary
-        var iDictionary = type.GetInterfaces().Where(t => {
-            return t.Name.StartsWith(nameof(IDictionary) + "`") &&
-                t.GenericTypeArguments.Length == 2 &&
-                t.GenericTypeArguments[0] == typeof(string);
-        }).FirstOrDefault();
-        if (iDictionary != null) {
-            var keyType = iDictionary.GenericTypeArguments[0];
-            var valueType = iDictionary.GenericTypeArguments[1];
-            var col = Activator.CreateInstance(type);
-            var addMethod = col.GetType().GetMethod("Add", new Type[] { keyType, valueType });
-
-            foreach (var (key, item) in obj) {
-                var value = ConvertItemToValue(valueType, item);
-                addMethod.Invoke(col, new object[] { key, value });
-            }
-
-            return col;
-        }
-
-        var result = Activator.CreateInstance(type);
-        FillFields(type, result, obj);
-        return result;
-    }
-
-    public object Deserialize(Type type, OMCLArray array) {
-        // arrays
-        if (type.IsArray) {
-            var elemType = type.GetElementType();
-            var arr = Array.CreateInstance(elemType, array.Length);
-
-            for (int i = 0; i < arr.Length; i++) {
-                var item = array[i];
-                var value = ConvertItemToValue(elemType, item);
-                arr.SetValue(value, i);
-            }
-
-            return arr;
-        }
-
-        // collections
-        var iCollection = type.GetInterfaces().Where(t => t.Name.StartsWith(nameof(ICollection) + "`") && t.GenericTypeArguments.Length == 1).FirstOrDefault();
-        if (iCollection != null) {
-            var elemType = iCollection.GenericTypeArguments[0];
-            var col = Activator.CreateInstance(type);
-            var addMethod = col.GetType().GetMethod("Add", new Type[] { elemType });
-
-            foreach (var item in array) {
-                var value = ConvertItemToValue(elemType, item);
-                addMethod.Invoke(col, new object[] { value });
-            }
-
-            return col;
-        }
-
-        // other types
-        if (_arrayConverters.ContainsKey(type)) {
-            var conv = _arrayConverters[type];
-            var collection = conv.CreateInstance();
-            foreach (var item in array) {
-                var value = ConvertItemToValue(conv.ElementType, item);
-                conv.AddValue(collection, value);
-            }
-            return collection;
-        }
-
-        throw new Exception($"Failed to deserialize array into type '{type.FullName}'. Please provide a custom converter if you want this type to be deserializable.");
     }
 
     private void FillFields<T>(T result, OMCLObject obj) {
@@ -149,10 +88,10 @@ public class Deserializer {
         
         switch (sourceType) {
         case OMCLItem.OMCLItemType.None:
-            return null;
+            return ConvertNone(targetType, tags);
 
         case OMCLItem.OMCLItemType.String:
-            return item.AsString();
+            return ConvertString(targetType, tags, item.AsString());
             
         case OMCLItem.OMCLItemType.Int:
             return Convert.ChangeType(item.AsInt(), targetType);
@@ -164,14 +103,133 @@ public class Deserializer {
             return Convert.ChangeType(item.AsFloat(), targetType);
 
         case OMCLItem.OMCLItemType.Object:
-            return Deserialize(targetType, item.AsObject());
+            return ConvertObject(targetType, item.AsObject());
 
         case OMCLItem.OMCLItemType.Array:
-            return Deserialize(targetType, item.AsArray());
+            return ConvertArray(targetType, item.AsArray());
 
         default:
             throw new NotImplementedException();
         }
+    }
+
+    public object ConvertNone(Type type, List<string> tags) {
+        // check if the type is nullable
+        if (type.IsClass)
+            return null;
+
+        bool isNullable = Nullable.GetUnderlyingType(type) != null;
+        if (isNullable)
+            return null;
+
+        throw new Exception($"Failed to deserialize none into type '{type.FullName}'. The type is not nullable.");
+    }
+
+    public object ConvertString(Type type, List<string> tags, string str) {
+        // check for custom converters
+        if (_stringConverters.ContainsKey(type)) {
+            var conv = _stringConverters[type];
+            return conv.ConvertString(tags, str);
+        }
+
+        // handle string -> string
+        if (type == typeof(string))
+            return str;
+
+        // enums
+        if (type.IsEnum) {
+            try {
+                return Enum.Parse(type, str, false);
+            }
+            catch (Exception e) {
+                var values = Enum.GetNames(type);
+                throw new Exception($"Failed to deserialize string into enum '{type.FullName}'. The string '{str}' does not represent a valid enum value. Possible values are: [{string.Join(", ", values)}]");
+            }
+        }
+
+        // check if type is nullable
+        var nullable = Nullable.GetUnderlyingType(type);
+        if (nullable != null) {
+            return ConvertString(nullable, tags, str);
+        }
+
+        try {
+            var value = Convert.ChangeType(str, type);
+            return value;
+        }
+        catch (Exception e) {
+            throw new Exception($"Failed to deserialize string into type '{type.FullName}'. Please provide a custom converter if you want this type to be deserializable.");
+        }
+    }
+
+    public object ConvertObject(Type type, OMCLObject obj) {
+        // check if its a dictionary
+        var iDictionary = type.GetInterfaces().Where(t => {
+            return t.Name.StartsWith(nameof(IDictionary) + "`") &&
+                t.GenericTypeArguments.Length == 2 &&
+                t.GenericTypeArguments[0] == typeof(string);
+        }).FirstOrDefault();
+        if (iDictionary != null) {
+            var keyType = iDictionary.GenericTypeArguments[0];
+            var valueType = iDictionary.GenericTypeArguments[1];
+            var col = Activator.CreateInstance(type);
+            var addMethod = col.GetType().GetMethod("Add", new Type[] { keyType, valueType });
+
+            foreach (var (key, item) in obj) {
+                var value = ConvertItemToValue(valueType, item);
+                addMethod.Invoke(col, new object[] { key, value });
+            }
+
+            return col;
+        }
+
+        var result = Activator.CreateInstance(type);
+        FillFields(type, result, obj);
+        return result;
+    }
+
+    public object ConvertArray(Type type, OMCLArray array) {
+        // handle custom converters
+        if (_arrayConverters.ContainsKey(type)) {
+            var conv = _arrayConverters[type];
+            var collection = conv.CreateInstance();
+            foreach (var item in array) {
+                var value = ConvertItemToValue(conv.ElementType, item);
+                conv.AddValue(collection, value);
+            }
+            return collection;
+        }
+
+        // arrays
+        if (type.IsArray) {
+            var elemType = type.GetElementType();
+            var arr = Array.CreateInstance(elemType, array.Length);
+
+            for (int i = 0; i < arr.Length; i++) {
+                var item = array[i];
+                var value = ConvertItemToValue(elemType, item);
+                arr.SetValue(value, i);
+            }
+
+            return arr;
+        }
+
+        // collections
+        var iCollection = type.GetInterfaces().Where(t => t.Name.StartsWith(nameof(ICollection) + "`") && t.GenericTypeArguments.Length == 1).FirstOrDefault();
+        if (iCollection != null) {
+            var elemType = iCollection.GenericTypeArguments[0];
+            var col = Activator.CreateInstance(type);
+            var addMethod = col.GetType().GetMethod("Add", new Type[] { elemType });
+
+            foreach (var item in array) {
+                var value = ConvertItemToValue(elemType, item);
+                addMethod.Invoke(col, new object[] { value });
+            }
+
+            return col;
+        }
+
+        throw new Exception($"Failed to deserialize array into type '{type.FullName}'. Please provide a custom converter if you want this type to be deserializable.");
     }
 }
 
